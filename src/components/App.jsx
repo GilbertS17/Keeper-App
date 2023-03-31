9c489c17106c3b56b499e67490aa72be54ad475c
gilbert sahyoun <gilbertsahyoun.business@gmail.com>
2023-03-31 11:26:25 +0300

    Render components dynamically from notes.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+const card = notes.map((item) => {
+  return <Note key={item.key} title={item.title} content={item.content} />;
+});
 
 function App() {
   return (
     <>
       <Header />
-      <Note />
+      {card}
       <Footer />
     </>
   );
